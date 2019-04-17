e8d584e19005bb1a3ba0b9f338cdf48ad81705ea
SYSINF\adai104 <murier.simon@gmail.com>
2019-04-17 15:06:10 +0200

    Add helper hashing

---
+++ Seven/SevenLib/Helpers/HashHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenLib.Helpers
+{
+    public class HashHelper
+    {
+        const String salt = "iloveturtles";
+        const String pepper = "iliketrains";
+
+        public static String HashString(String plaintext)
+        {
+            plaintext = salt + plaintext + pepper;
+
+            byte[] bytes = Encoding.UTF8.GetBytes(plaintext);
+
+            SHA256Managed hashString = new SHA256Managed();
+            byte[] hash = hashString.ComputeHash(bytes);
+
+            String hashed = String.Empty;
+
+            foreach (byte B in hash)
+            {
+                hashed += String.Format("{0:x2}", B);
+            }
+
+            return hashed;
+        }
+    }
+}
